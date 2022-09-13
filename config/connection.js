ef6e20fbf5c291b10d3b1502665ec09d7f8907b9
Jamlit37 <james.v.little@hotmail.com>
2022-09-13 18:27:21 +1000

    should make the heroku app work

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,15 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-   sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
+      host: 'localhost',
       dialect: 'mysql',
-      port: process.env.PORT || 3306
-    }
+      port: 3306,
+    },
   );
 }
 
