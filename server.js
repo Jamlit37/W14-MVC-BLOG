c56af3ffbfc9019a37077c4cf80439e43d2149f6
Jamlit37 <james.v.little@hotmail.com>
2022-09-15 19:10:56 +1000

    fixed loading issues after feedback

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -30,6 +31,7 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -40,6 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT);
+  app.listen(PORT, () => console.log('Now listening'));
 });
-
